41495881524497c3af7f1d57909f07324934b363
Air111 <1796389814@qq.com>
2020-12-16 21:21:04 +0800

    add asm and change initfile

--- tools/bin2mif.py
+++ tools/bin2mif.py
@@ -1,6 +1,6 @@
 import os
 
-fr = open('project/init_files/t1', 'rb')
+fr = open('project/init_files/t2', 'rb')
 fw = open('project/init_files/instr_ram.mif', 'w')
 depth = 1024
 width = 32
@@ -10,7 +10,7 @@ fw.write('ADDRESS_RADIX=HEX;\n')
 fw.write('DATA_RADIX=HEX;\n\n')
 fw.write('CONTENT BEGIN\n')
 s = 0
-for i in range(os.path.getsize('project/init_files/t1')):
+for i in range(os.path.getsize('project/init_files/t2')):
     tmp = fr.read(4)
     num = int.from_bytes(tmp, 'little')
     fw.write('   {:0>3X} : {:0>8X} ;\n'.format(s, num))
